072dd681cb711c6ebbfef3f10cbc24bef5ed8870
KarinaKhairunnisap <121793572+KarinaKhairunnisap@users.noreply.github.com>
2023-01-10 20:51:58 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,29 +1,45 @@
 #include<iostream>
 using namespace std;
+int findSmallest (int[],int);
 int main ()
 {
     int myarray[500000];
-
-    cout<<"\nInput list is \n";
-    for(int i=0; i<500000; i++)
+    int pos,temp,pass=0;
+    cout<<"\n Input list of elements to be Sorted\n" << endl;
+    for(int i=0; i<50000; i++)
     {
         myarray[i] = rand();
-        cout <<myarray[i]<<"\t";
+        cout<<myarray[i]<<"\t";
     }
-    for(int k=1; k<500000; k++)
+    for(int i=0; i<50000; i++)
     {
-        int temp = myarray[k];
-        int j= k-1;
-        while(j>=0 && temp <= myarray[j])
+        pos = findSmallest (myarray,i);
+        temp = myarray[i];
+        myarray[i]=myarray[pos];
+        myarray[pos] = temp;
+        pass++;
+    }
+    cout << endl <<"\n Sorted list of elements is\n";
+    for(int i=0; i<50000; i++)
+    {
+        cout<<myarray[i]<<"\t";
+    }
+    cout<<"\nNumber of passes required to sort the array: " <<pass;
+    return 0;
+}
+
+int findSmallest(int myarray[],int i)
+{
+    int ele_small,position,j;
+    ele_small = myarray[i];
+    position = i;
+    for(j=i+1; j<50000; j++)
+    {
+        if(myarray[j]<ele_small)
         {
-            myarray[j+1] = myarray[j];
-            j = j-1;
+            ele_small = myarray[j];
+            position=j;
         }
-        myarray[j+1] = temp;
     }
-    cout<<"\nSorted list is \n";
-    for(int i=0; i<500000; i++)
-    {
-        cout <<myarray[i]<<"\t";
-    }
+    return position;
 }
